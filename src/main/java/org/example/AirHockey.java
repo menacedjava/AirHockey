3610bbd65f73d5b0da29766af9fbe0fe53978700
Madumarov Davron <menaceteacher@gmail.com>
2025-02-11 01:52:48 +0500

    add

--- src/main/java/org/example/AirHockey.java
+++ src/main/java/org/example/AirHockey.java
@@ -9,52 +9,52 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class AirHockey extends JPanel implements ActionListener, KeyListener {
-//    private static final int WIDTH = 800, HEIGHT = 500;
-//    private static final int PADDLE_WIDTH = 20, PADDLE_HEIGHT = 100;
-//    private static final int BALL_SIZE = 20;
-//
-//    private int paddle1Y = HEIGHT / 2 - PADDLE_HEIGHT / 2;
-//    private int paddle2Y = HEIGHT / 2 - PADDLE_HEIGHT / 2;
-//    private int paddleSpeed = 10;
-//
-//    private int ballX = WIDTH / 2 - BALL_SIZE / 2;
-//    private int ballY = HEIGHT / 2 - BALL_SIZE / 2;
-//    private int ballVelocityX = 5, ballVelocityY = 3;
-//
-//    private int score1 = 0, score2 = 0;
-//
-//    private Timer timer;
+    private static final int WIDTH = 800, HEIGHT = 500;
+    private static final int PADDLE_WIDTH = 20, PADDLE_HEIGHT = 100;
+    private static final int BALL_SIZE = 20;
 
-//    public AirHockey() {
-//        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
-//        this.setBackground(Color.BLACK);
-//        this.setFocusable(true);
-//        this.addKeyListener(this);
-//
-//        timer = new Timer(1000 / 60, this);
-//        timer.start();
-//    }
+    private int paddle1Y = HEIGHT / 2 - PADDLE_HEIGHT / 2;
+    private int paddle2Y = HEIGHT / 2 - PADDLE_HEIGHT / 2;
+    private int paddleSpeed = 10;
 
-//    @Override
-//    protected void paintComponent(Graphics g) {
-//        super.paintComponent(g);
-//
-//        // Chiziq
-//        g.setColor(Color.WHITE);
-//        g.drawLine(WIDTH / 2, 0, WIDTH / 2, HEIGHT);
-//
-//        // O'yinchilarning ballari
-//        g.setFont(new Font("Arial", Font.BOLD, 30));
-//        g.drawString(String.valueOf(score1), WIDTH / 4, 50);
-//        g.drawString(String.valueOf(score2), WIDTH * 3 / 4, 50);
-//
-//        // Raketkalar
-//        g.fillRect(50, paddle1Y, PADDLE_WIDTH, PADDLE_HEIGHT);
-//        g.fillRect(WIDTH - 70, paddle2Y, PADDLE_WIDTH, PADDLE_HEIGHT);
-//
-//        // Shayba
-//        g.fillOval(ballX, ballY, BALL_SIZE, BALL_SIZE);
-//    }
+    private int ballX = WIDTH / 2 - BALL_SIZE / 2;
+    private int ballY = HEIGHT / 2 - BALL_SIZE / 2;
+    private int ballVelocityX = 5, ballVelocityY = 3;
+
+    private int score1 = 0, score2 = 0;
+
+    private Timer timer;
+
+    public AirHockey() {
+        this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(this);
+
+        timer = new Timer(1000 / 60, this);
+        timer.start();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        // Chiziq
+        g.setColor(Color.WHITE);
+        g.drawLine(WIDTH / 2, 0, WIDTH / 2, HEIGHT);
+
+        // O'yinchilarning ballari
+        g.setFont(new Font("Arial", Font.BOLD, 30));
+        g.drawString(String.valueOf(score1), WIDTH / 4, 50);
+        g.drawString(String.valueOf(score2), WIDTH * 3 / 4, 50);
+
+        // Raketkalar
+        g.fillRect(50, paddle1Y, PADDLE_WIDTH, PADDLE_HEIGHT);
+        g.fillRect(WIDTH - 70, paddle2Y, PADDLE_WIDTH, PADDLE_HEIGHT);
+
+        // Shayba
+        g.fillOval(ballX, ballY, BALL_SIZE, BALL_SIZE);
+    }
 
 //    @Override
 //    public void actionPerformed(ActionEvent e) {
